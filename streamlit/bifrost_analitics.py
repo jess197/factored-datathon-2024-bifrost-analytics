23392ffb877f6f7af88054e60addd8a77652fdc5
Jess <jessicacostaesilva97@gmail.com>
2024-08-25 22:57:02 -0300

    added text

--- streamlit/bifrost_analitics.py
+++ streamlit/bifrost_analitics.py
@@ -12,11 +12,6 @@ actor2_dict = {
     'PALESTINE' : 'PSE'
 }
 
-actor2 = st.selectbox(
-    "Select the Actor 2",
-    ('UNITED STATES', 'ISRAEL', 'UKRAINE', 'RUSSIA', 'CHINA', 'PALESTINE'),
-)
-
 event_type_dict = {
     'Make a visit' : '042',
     'Host a visit' : '043',
@@ -25,13 +20,20 @@ event_type_dict = {
     'Praise or endorse' : '051'
 }
 
+st.write("Please select actor and event type to generate the news. Notice that the Actor 1 is always United States.")
+
+actor2 = st.selectbox(
+    "Select the Actor 2",
+    ('UNITED STATES', 'ISRAEL', 'UKRAINE', 'RUSSIA', 'CHINA', 'PALESTINE'),
+)
+
 event_type = st.selectbox(
     "Select the Event Type",
     ('Make a visit', 'Host a visit', 'Consult, not specified below', 'Make statement, not specificed below', 'Praise or endorse'),
 )
 
-if st.button("Generate New"):
-    st.write("Generating news for:", actor2, event_type)
+if st.button("Generate"):
+    st.write("Generating news for:", actor2, " and ", event_type)
 
     dtbricks = Databricks()
     news_generator = NewsGenerator()
