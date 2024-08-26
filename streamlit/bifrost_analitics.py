7a78dc72b5aa27c898e5bd3877e0a0e562206af5
Jess <jessicacostaesilva97@gmail.com>
2024-08-25 22:51:42 -0300

    added china as actor

--- streamlit/bifrost_analitics.py
+++ streamlit/bifrost_analitics.py
@@ -3,12 +3,18 @@ import streamlit as st
 from data.databricks import Databricks
 from data.news_generator import NewsGenerator
 
-dtbricks = Databricks()
-news_generator = NewsGenerator()
+actor2_dict = {
+    'UNITED STATES' : 'USA',
+    'ISRAEL' : 'ISR',
+    'UKRAINE' : 'UKR',
+    'RUSSIA' : 'RUS',
+    'CHINA' : 'CHN',
+    'PALESTINE' : 'PSE'
+}
 
 actor2 = st.selectbox(
     "Select the Actor 2",
-    ('USA', 'ISR', 'PSE', 'RUS', 'UKR'),
+    ('UNITED STATES', 'ISRAEL', 'UKRAINE', 'RUSSIA', 'CHINA', 'PALESTINE'),
 )
 
 event_type_dict = {
@@ -16,7 +22,7 @@ event_type_dict = {
     'Host a visit' : '043',
     'Consult, not specified below' : '040',
     'Make statement, not specificed below' : '010',
-    'Praise or endorse' : '051',
+    'Praise or endorse' : '051'
 }
 
 event_type = st.selectbox(
@@ -24,19 +30,22 @@ event_type = st.selectbox(
     ('Make a visit', 'Host a visit', 'Consult, not specified below', 'Make statement, not specificed below', 'Praise or endorse'),
 )
 
-if st.button("Generate Next"):
+if st.button("Generate New"):
     st.write("Generating news for:", actor2, event_type)
 
-    df = dtbricks.get_latest_news(actor2, event_type_dict[event_type])
+    dtbricks = Databricks()
+    news_generator = NewsGenerator()
+    
+    df = dtbricks.get_latest_news(actor2_dict[actor2], event_type_dict[event_type])
     news_body = dtbricks.get_news_body(df['GlobalEventID'][0])
     next_news = news_generator.get_next_news(df, news_body)
 
     col1, col2 = st.columns(2)
 
     with col1:
-        st.header("Today news:")
+        st.header("Original news:")
         st.write(news_body)
 
     with col2:
-        st.header("Next news:")
+        st.header("Generated(T+1) news:")
         st.write(next_news)
